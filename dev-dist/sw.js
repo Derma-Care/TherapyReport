8d849c95c2faa23a5c00969478691b4767dd0a61
BanavathS45 <prashanthr803@gmail.com>
2026-05-02 13:54:14 +0530

    Sw

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7hf1ima6egs"
+    "revision": "0.h9k92dk0914"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
